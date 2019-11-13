ea223739f3b76924c93016dc4caf9980ca500c41
Raymond Lin <raymond.lin@duke.edu>
2019-11-13 01:01:06 -0500

    added scrolling and changed menu

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// const ScrollReveal = window.ScrollReveal;
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null
+};
+window.sr = new ScrollReveal()
+console.log("HI");
+ScrollReveal().reveal('.intro', slideUp);
+sr.reveal('.intro', { delay: 250 });
+sr.reveal('.blurb', slideUp);
+console.log("HI");
