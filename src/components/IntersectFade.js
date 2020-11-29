46f821d3b93bbb963fcb9d3e45e51cf2875172a2
Raymond Lin <rlin264@gmail.com>
2020-11-28 22:06:18 -0500

    feature(intersectfade): cleanup and format

--- src/components/IntersectFade.js
+++ src/components/IntersectFade.js
@@ -1,20 +1,20 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import useIntersect from "./useIntersect";
 import Fade from "react-reveal/Fade";
 
-const buildThresholdArray = () => Array.from(Array(100).keys(), i => i / 100);
+const buildThresholdArray = () => Array.from(Array(100).keys(), (i) => i / 100);
 
-function IntersectFade(props){
-    const [ref, entry] = useIntersect({
-        threshold: buildThresholdArray()
-    });
-    return(
-        <div ref={ref} ratio={entry.intersectionRatio}>
-            <Fade {...props} top distance="10vh" when={entry.intersectionRatio>0.3}>
-                {props.children}
-            </Fade>
-        </div>  
-    );
+function IntersectFade(props) {
+  const [ref, entry] = useIntersect({
+    threshold: buildThresholdArray(),
+  });
+  return (
+    <div ref={ref}>
+      <Fade {...props} top distance="10vh" when={entry.intersectionRatio > 0.3}>
+        {props.children}
+      </Fade>
+    </div>
+  );
 }
 
 export default IntersectFade;
